166790b289902335e8f2180c3179fea623160b5b
Ivan Bogachev <sfi0zy@gmail.com>
2023-01-29 13:50:15 +0300

    Move terminal settings to the end

--- main.sh
+++ main.sh
@@ -6,6 +6,8 @@
 
 set -e
 
+sudo ./install.sh
+
 palette1="#121212:#ff005f:#00af5f:#ffd787:#0087ff:#ff00ff:#00d4ff:#e4e4e4"
 palette2="#1c1c1c:#ff005f:#00ad5f:#ffd787:#0087ff:#ff00ff:#00d4ff:#00875f"
 
@@ -16,8 +18,6 @@ gsettings set io.elementary.terminal.settings cursor-color "#e4e4e4"
 gsettings set io.elementary.terminal.settings cursor-shape "I-Beam"
 gsettings set io.elementary.terminal.settings follow-last-tab "true"
 gsettings set io.elementary.terminal.settings theme "custom"
-
-sudo ./install.sh
 
 echo "Restart your computer to finish workspace setup"
 notify-send "Restart your computer to finish workspace setup"
